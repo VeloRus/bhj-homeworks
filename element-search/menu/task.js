6cbb421994005ecf3f158489f0b0dec6a42d1ac0
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-08-23 23:23:58 +0300

    menu ready

---
+++ element-search/menu/task.js
@@ -0,0 +1,16 @@
+const links = document.querySelectorAll('.menu__link');
+
+links.forEach((item) => {
+    item.onclick = function (event) {
+        if (item.closest('.menu__item').querySelector(".menu_sub")) {
+            const activeMenu = item.closest('.menu_main').querySelectorAll('.menu_active');
+                if (activeMenu.length > 0) {
+                    activeMenu.forEach((item) => item.classList.remove('menu_active'));
+                }
+
+            item.closest('.menu__item').querySelector(".menu_sub").classList.toggle("menu_active");
+
+            return false
+        }
+    };
+});

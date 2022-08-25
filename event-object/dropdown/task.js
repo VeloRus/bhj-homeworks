a619f0c5c7cbf6d21bc5bf1005724337e9aba6aa
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-08-25 23:29:13 +0300

    OMG

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,17 @@
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownList = document.querySelector('.dropdown__list');
+const dropdownItem =  Array.from(document.querySelectorAll(".dropdown__item"));
+
+dropdownValue.addEventListener('click', () => {
+    if (dropdownList.className == 'dropdown__list') {
+    dropdownList.classList.add('dropdown__list_active')
+    } else { dropdownList.className = 'dropdown__list'}
+ });
+
+ for (let item of dropdownItem) {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
+        dropdownValue.textContent = dropdownItem[dropdownItem.indexOf(item)].querySelector('.dropdown__link').textContent;
+        dropdownList.className = 'dropdown__list';
+    })
+};

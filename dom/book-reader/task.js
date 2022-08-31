a5a2e27271e9f6fdc3bebcfe10970fed52af8dd4
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-08-31 19:45:33 +0300

    fixed problem

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -2,21 +2,18 @@ const fontSize = document.getElementsByClassName('font-size');
 const book = document.getElementById('book');
 
 
+
 for (let item of fontSize) {
 	item.onclick = () => {
+		deleteClass();
+        item.classList.add('font-size_active');
 		if (item.getAttribute('data-size') === 'small') {
-			deleteClass();
-			item.classList.add('font-size_active');
 			book.classList.remove('font-size_big');
 			book.classList.add('font-size_small');
 		} else if (item.getAttribute('data-size') === 'big') {
-			deleteClass();
-			item.classList.add('font-size_active');
 			book.classList.remove('font-size_small');
 			book.classList.add('font-size_big');
 		} else {
-			deleteClass();
-			item.classList.add('font-size_active');
 			book.classList.remove('font-size_big');
 			book.classList.remove('font-size_small');
 		};

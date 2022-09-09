ddd5c96bd90f185484787973951de6121ebc3810
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-09-09 22:09:44 +0300

    tooltip ready

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,20 @@
+const hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+
+hasTooltip.forEach(element => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault();
+    const tooltip = document.querySelector('.tooltip');
+    if(tooltip != null) {
+        tooltip.remove();
+    }
+    addTip(element);
+  })
+})
+
+function addTip(item) { 
+  item.insertAdjacentHTML('afterEnd',`<div class="tooltip tooltip_active">${item.title}</div>`);
+  const tooltipNew = document.querySelector('.tooltip');
+  const coordinates = item.getBoundingClientRect();
+  tooltipNew.setAttribute('data-position', 'bottom');
+  tooltipNew.setAttribute('style', `left: ${coordinates.left}px; top: ${coordinates.bottom}px;`);
+}

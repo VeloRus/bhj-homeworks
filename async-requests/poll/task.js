2c8041db8fd155ea3cc2d54bfe0145d434755a35
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-09-12 22:38:15 +0300

    poll is ready

---
+++ async-requests/poll/task.js
@@ -0,0 +1,25 @@
+const pollTitle = document.querySelector('.poll__title');
+const pollAnswers = document.querySelector('.poll__answers');
+
+const xhr = new XMLHttpRequest;
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+xhr.send();
+
+xhr.addEventListener('readystatechange', () => {
+
+    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+        const obj = JSON.parse(xhr.responseText);
+        const arrAnswers = obj.data.answers;
+
+        pollTitle.insertAdjacentHTML('afterbegin', obj.data.title);
+
+        arrAnswers.forEach(element => {
+            pollAnswers.innerHTML += 
+            `<button class="poll__answer" style = "margin-right: 10px">${element}</button>`
+        });        
+    }    
+})
+
+pollAnswers.addEventListener('click', () => {
+        alert('Спасибо, ваш голос засчитан!')
+    })

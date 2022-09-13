e2a7010a7b8a028aec687ecb410933fed59ed1b0
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2022-09-14 02:13:01 +0300

    HW

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,21 +1,23 @@
-const hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+let hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+let div = document.createElement('div');
 
-hasTooltip.forEach(element => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault();
-    const tooltip = document.querySelector('.tooltip');
-    if(tooltip != null) {
-        tooltip.remove();
-        return;
+hasTooltip.forEach((element) => {
+  element.addEventListener('click', (event) => { 
+  event.preventDefault();
+  div.classList.add('tooltip');
+  if (div.classList.contains('tooltip_active')) {
+      div.classList.remove('tooltip_active');
+        if (div.textContent == element.title) {
+        return false;
+        }
     }
-    addTip(element);
-  })
-})
 
-function addTip(item) { 
-  item.insertAdjacentHTML('afterEnd',`<div class="tooltip tooltip_active">${item.title}</div>`);
-  const tooltipNew = document.querySelector('.tooltip');
-  const coordinates = item.getBoundingClientRect();
-  tooltipNew.setAttribute('data-position', 'bottom');
-  tooltipNew.setAttribute('style', `left: ${coordinates.left}px; top: ${coordinates.bottom}px;`);
-}
+  div.textContent = element.title
+  element.append(div)
+  div.classList.add('tooltip_active')
+  const position = element.getBoundingClientRect();
+  div.style.left = position.left + 'px';
+  div.style.top = position.bottom + 5 + 'px';
+  })    
+});
+
